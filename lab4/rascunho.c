9558f685d6b9658b1869be51b5758fd3341d5633
andresantosr <andresanro03@gmail.com>
2024-03-31 16:52:23 -0300

    lab04

--- lab4/rascunho.c
+++ lab4/rascunho.c
@@ -1,54 +1,4 @@
-int read(int __fd, const void *__buf, int __n){
-    int ret_val;
-  __asm__ __volatile__(
-    "mv a0, %1           # file descriptor\n"
-    "mv a1, %2           # buffer \n"
-    "mv a2, %3           # size \n"
-    "li a7, 63           # syscall write code (63) \n"
-    "ecall               # invoke syscall \n"
-    "mv %0, a0           # move return value to ret_val\n"
-    : "=r"(ret_val)  // Output list
-    : "r"(__fd), "r"(__buf), "r"(__n)    // Input list
-    : "a0", "a1", "a2", "a7"
-  );
-  return ret_val;
-}
-
-void write(int __fd, const void *__buf, int __n)
-{
-  __asm__ __volatile__(
-    "mv a0, %0           # file descriptor\n"
-    "mv a1, %1           # buffer \n"
-    "mv a2, %2           # size \n"
-    "li a7, 64           # syscall write (64) \n"
-    "ecall"
-    :   // Output list
-    :"r"(__fd), "r"(__buf), "r"(__n)    // Input list
-    : "a0", "a1", "a2", "a7"
-  );
-}
-
-void exit(int code)
-{
-  __asm__ __volatile__(
-    "mv a0, %0           # return code\n"
-    "li a7, 93           # syscall exit (64) \n"
-    "ecall"
-    :   // Output list
-    :"r"(code)    // Input list
-    : "a0", "a7"
-  );
-}
-
-void _start()
-{
-  int ret_code = main();
-  exit(ret_code);
-}
-
-#define STDIN_FD  0
-#define STDOUT_FD 1
-
+#include <stdio.h>
 
 int strlen(char *string) {
     char aux;
@@ -234,21 +184,23 @@ int main()
     char number[32];
 
     /* Read up to 20 bytes from the standard input into the str buffer */
-    int n = read(STDIN_FD, number, 32);
+    
+    scanf("%d", &number);
+
 
     char base[20] = check_base(&number); 
     char * dresult, *bresult, *c2result;
 
     dresult     = to_decimal(&number, base);
     bresult     = to_binary(&number);
-    c2result    = to_c2decimal(&number);
-    change_endianness(&number);
-    to_hexa(&number);
-    to_octal(&number);
+    //c2result    = to_c2decimal(&number);
+    //change_endianness(&number);
+    //to_hexa(&number);
+    //to_octal(&number);
 
-
+    printf("%s\n", bresult);
+   
 
     /* Write n bytes from the str buffer to the standard output */
-    write(STDOUT_FD, number, n);
     return 0;
 }
