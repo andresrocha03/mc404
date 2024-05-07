b47ff1f6885591d6c23d9c38d1045ae6df89c62a
Andre Santos Rocha <ra235887@drummond.lab.ic.unicamp.br>
2024-05-07 16:28:49 -0300

    lab7

--- lab07/coord.s
+++ lab07/coord.s
@@ -13,27 +13,81 @@ main:
     sw ra, 0(sp)
 
     # ler entrada 
-    la a1, coord_address
-    jal read
-    mv s0, a1
-
-    la a1, time_address
-    jal read
-    mv s1, a1
     
-    # calcular da
+    li a2, 12           # size - reads 12 bytes
+    jal read
+    mv s0, a1           # coord
 
-    # 
-    # obter os digitos da string salva em s0
-    jal get_digit
+    li a2, 20           # size - reads 20 bytes
+    jal read
+    mv s1, a1           # TR, TA, TA, TB, TC
 
-    # get int number from 4 str
-    jal to_decimal 
+   
+    mv s2, a0
 
-    # calcular raiz quadrada
-    jal sqrt 
+    # obter numeros inteiros a partir da entrada
+        # Xc
+        jal get_coord 
+        mv s2, a0
+        # Yb
+        addi s0, 6 # atualizar ponteiros
+        jal get_coord 
+        mv s3, a0
+        # Tr
+        mv s0,s1 # mover de s1 para s0, apenas para reaproveitar as funcoes...
+        jal get_time
+        mv s4, a0
+        # Ta
+        addi s0,5
+        jal get_time
+        mv s5, a0
+        # Tb
+        addi s0,5
+        jal get_time
+        mv s6, a0
+        # Tc
+        addi s0,5
+        jal get_time
+        mv s7, a0
+
+    li t1, 3
+    li t2, 10
+    # calcular Da : (tr - ta) * 3/ 10
+    sub s8, s4, s5
+    mul s8, s8, t1
+    div s8, s8, t2
+
+    # calcular Db
+    sub s9, s4, s6
+    mul s9, s9, t1
+    div s9, s9, t2
+
+    # calcular Dc
+    sub s9, s4, s7
+    mul s9, s9, t1
+    div s9, s9, t2
+
+    # calcular y
+    mul t1, s8, s8 # da²
+    mul t2, s3, s3 # yb²
+    mul t3, s9, s9 # db²
+    li t5, 2
+    mul t4, t5, s3
+    add s10, t1, t2
+    sub s10, s10, t3
+    div s10, s10, t4
+
+    # calcular x = (da²+xc²-dc²)/(2xc)
+    mul t5, s2, s2 # Xc²
+    mul t6, s9, s9 # dc²
+    li t3, 2
+    mul t2, t3, xc # 2xc
+    add s11, t1, t5
+    sub s11, s11, t6
+    div s11, s11, t2
 
     # salvar resposta em result
+    mv a0, s11
     jal save
 
     # atualizar ponteiro do input
@@ -58,7 +112,7 @@ main:
 read:
 
     li a0, 0            # file descriptor = 0 (stdin)
-    li a2, 20           # size - Reads 20 bytes.
+    la a1, input_address
     li a7, 63           # syscall read (63)
     ecall
    
@@ -74,11 +128,11 @@ write:
 
 get_digit:
     # input string em s0
-    # output digitos da string em a0,a1,a2,a3
-    lbu a0, 0(s0)
+    # output digitos da string em a0(sinal),a1,a2,a3,a4
     lbu a1, 1(s0)
     lbu a2, 2(s0)
     lbu a3, 3(s0)
+    lbu a4, 4(s0)
     ret
 
 to_decimal:
@@ -87,28 +141,67 @@ to_decimal:
     # output: numero inteiro em a0
     
     # converter string para int
-    addi a0, a0, -48
     addi a1, a1, -48
     addi a2, a2, -48
     addi a3, a3, -48
+    addi a4, a4, -48
     
     # multiplicar pelas potencias de 10
     li t5, 1000
-    mul a0, a0, t5
-    li t5, 100
     mul a1, a1, t5
-    li t5, 10
+    li t5, 100
     mul a2, a2, t5
-    li t5, 1
+    li t5, 10
     mul a3, a3, t5
+    li t5, 1
+    mul a4, a4, t5
     
     li a5, 0
-    add a5, a5, a0
     add a5, a5, a1
     add a5, a5, a2
     add a5, a5, a3
-    li a0, 0
-    mv a0, a5
+    add a5, a5, a4
+    li t1, 45
+    bne a0, t1, endif
+        li t1, -1
+        mul a5, a5, a1
+    endif:
+        li a0, 0
+        mv a0, a5
+    ret
+
+get_coord:
+    # ler uma string de 4 digitos com sinal, salva em s0
+    # retorna numero inteiro em a0
+    addi sp,sp,-4
+    sw ra, 0(sp)
+
+    # obter os digitos da string salva em s0, salvos em a0 (sinal), a1, a2, a3, a4
+    lbu a0, 0(s0) # get sign
+    jal get_digit
+
+    # get int number from 4 str and save it in a0
+    jal to_decimal 
+
+    lw ra,0(sp)
+    addi sp, sp, 4
+    ret
+
+get_time:
+    # ler uma string de 4 digitos sem sinal, salva em s1
+    # retorna numero inteiro em a0
+    addi sp,sp,-4
+    sw ra, 0(sp)
+
+    # obter os digitos da string salva em s0, salva-los em a0 (sinal), a1, a2, a3, a4
+    li a0, 43
+    jal get_digit
+
+    # get int number from 4 str and save it in a0
+    jal to_decimal 
+
+    lw ra,0(sp)
+    addi sp, sp, 4
     ret
 
 sqrt:
@@ -116,18 +209,18 @@ sqrt:
     # input: numero decimal salvo em a0
     # output: raiz quadrada aproximada em a0
     li t1, 0    # i do for
-    li t2, 10   # limite do for
+    li t2, 21   # limite do for
     li t3, 0    # y/k
     li t4, 0    # k 
     li t5, 2    
     
     div t4, a0, t5 # k = y/2
     loop_sqrt:
-        bge t1, t2, lab_sqrt    # Sai do laço se i >= 10
-        div t3, a0, t4      # y/k
-        add t4, t4, t3      # k + y/k
-        div t4, t4, t5       # (k + y/k)/2
-        addi t1, t1, 1      # i <= i+1
+        bge t1, t2, lab_sqrt    # Sai do laço se i >= 21
+        div t3, a0, t4          # y/k
+        add t4, t4, t3          # k + y/k
+        div t4, t4, t5          # (k + y/k)/2
+        addi t1, t1, 1          # i <= i+1
         j loop_sqrt
 
     lab_sqrt:
@@ -138,7 +231,8 @@ save:
     # salva um inteiro em result, como uma string no formato DDDD
     # input: numero inteiro armazenado em a0, ponteiro para result armazenado em s1
     # output: resultado salvo em result
-    //li a0, 0
+    # verificar 
+    li a, 0
     li t5, 32
     
     li t3, 1000 # divisor
@@ -169,9 +263,8 @@ save:
     ret
 
 .bss
-time_address: .skip 0x20  # buffer
-coord_address: .skip 0x20
+input_address: .skip 0x20  # buffer
 result: .skip 0x20
 
-# calcular da : (tr - ta) * 3/ 10
+
 # 
