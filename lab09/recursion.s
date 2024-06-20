532798ffed4d17bd5faa7f2103b00e5d68131d66
Andre Santos Rocha <ra235887@tolkien.lab.ic.unicamp.br>
2024-06-20 15:29:45 -0300

    lab09 concluido

--- lab09/recursion.s
+++ lab09/recursion.s
@@ -21,12 +21,8 @@ main:
    
     jal to_decimal
 
-    la t4, head_node
-    li t1, 10001
-    li a1, -1
     jal find_node
 
-    la s1, result
     jal save
 
     jal write
@@ -53,7 +49,7 @@ write:
 
 to_decimal:
     # converter uma string para um numero decimal
-    # input: endereco da string em s0
+    # input: endereco da string loop_digiem s0
     # output: numero inteiro em a0
     
     # converter string para int
@@ -87,84 +83,88 @@ to_decimal:
         mul a0, a0, t4
         ret
 
+
 find_node:
-    # recebe endereco em t4 e um inteiro em a0
-    # devolve o numero do nó em a1
+    # recebe inteiro em a0
+    # devolve o numero do nó em a0
 
-    // t4 é no atual
-    // t1 é a soma atual 
-    // a0 é o inteiro que estamos procurando
-    // a2 é o 0, pra ver se chegamos no ultimo no
-    beq t1, a0, found
-   
-    li a2, 0 
-    beq t4, a2, no_result_found
+    la a1, head_node // endereco do primeiro no
+    li t4, 0 //contador de nos
+    li t1, 10001 //valor soma total
     
-    lw t1, 0(t4)
-    lw t2, 4(t4)
-    lw t3, 8(t4)
+    search_loop:
+        lw t1, 0(a1)
+        lw t2, 4(a1)
+        lw t3, 8(a1)
 
-    add t1, t2, t1
-    add t1, t3, t1
-
-    lw t4, 12(t4)
-    addi a1, a1, 1
-    j find_node
+        add t1, t2, t1
+        add t1, t3, t1
+        beq t1, a0, found
+        
+        lw a1, 12(a1)
+        beq a1, zero, no_result_found
+        
+        addi t4, t4, 1
+        j search_loop
     
     found:
+        mv a0, t4  
         ret
     
     no_result_found:
-        li a1, -1
+        li a0, -1
         ret
 
 save:
     # salva um inteiro em result, como uma string
-    # input: numero inteiro armazenado em a1, ponteiro para result armazenado em s1
+    # input: numero inteiro armazenado em a0
     # output: resultado salvo em result
    
-    // t5 é o \n
-    // t6 é o 
-    // t3 é -1, caso em que nenhum no foi encontrado
-    li t5, 1
-    li t6, 0
-    li t3, -1
-    
-    bne a1, t3, yes_result
-    no_result:
-        li t4, 45
-        sb t4, 0(s1)
-        li t4, 49
-        sb t4, 1(s1)
-        sb t5, 2(s1)
+    // se a0 for -1, nao foi encontrado nenhum no
+    li t1, -1
+    la s1, result
+    bne a0, t1, yes_result
+        li t1, 45
+        sb t1, 0(s1) // colocar o -
+        li t1, 49
+        sb t1, 1(s1) // colocar o 1
+        li t1, 10
+        sb t1, 2(s1) // colocar o \n
         ret
    
-    yes_result:
-    mv t4, a1 // armazenar em t4 temporariamente para contar o numero de digitos
-
+    yes_result: 
+    mv t1, a0 // armazenar em t1 temporariamente para contar o numero de digitos
     li t2, 0
-    loop_digit:    
-        div t4, t4, t5  # retirar um digito do numero e guardar o quociente
-        addi t2, t2,  1
-        beq t4, t6, potencia
-        j loop_digit
+    li t3, 10
+    conta_digito:    
+        div t1, t1, t3  # retirar um digito do numero e guardar o quociente
+        addi t2, t2,  1 // contabilizar o digito
+        beq t1, zero, end_contagem
+        j conta_digito
+
+    // numero de digitos esta salvo em t2
+    end_contagem:
     
     li t1, 1
-    li t6, 10
+    li t3, 1
+    li t4, 10
     potencia:
         beq t1, t2, end_potencia
-        mul t5, t5, t6
+        mul t3, t3, t4
         addi t1, t1, 1
         j potencia
 
+    //o valor do meu divisor esta guardado em t3
     end_potencia:
+    
     li t1, 0
+    li t5, 10
     loop_save:
         beq t1, t2, end_all
-        
-        div t4, a1, t5  # retirar um digito do numero e guardar o quociente
-        rem a1, a1, t5  # guardar resto da divisao 
-        div t5, t5, t6
+
+        div t4, a0, t3  # retirar um digito do numero e guardar o quociente
+        rem a0, a0, t3  # guardar resto da divisao 
+        div t3, t3, t5  # dividir divisor por 10
 
         addi t4, t4, 48 # converter para string
         sb t4, 0(s1)    # escrever digito em result na ordem inversa   
@@ -176,6 +176,7 @@ save:
     end_all:
         sb t5, 0(s1)
         ret
+
 
 .bss
 input_address: .skip 0x10 # buffer
